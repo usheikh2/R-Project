5ab9393c4ce96838d7fdb8b68997f9c304bd95bc
Umber Sheikh <umbersheikh@actumbersheikh.myfiosgateway.com>
2022-01-28 15:23:15 -0500

    import and read instacart data set

--- data_cleanup.R
+++ data_cleanup.R
@@ -1,2 +1,24 @@
-print("Hi OR568 Team! This is a test message to verify that you see a code change")
-print("Push a commit from this file with your name so we know we're all linked to the same file")
+install.packages("purrr")
+install.packages("tidyverse")
+library(tidyverse)
+library(purrr)
+
+#Replace with your directory 
+file_path <- fs::dir_ls("~/Desktop/u/OR 568/OR Project/archive")
+
+#Reads all csv files in that path 
+file_path %>%
+  map(function (path){
+    read_csv(path)
+    
+  })
+file_path  
+
+#Read CSV data tables in file_path
+aisles = read.csv(file_path[[1]])
+departments = read.csv(file_path[[2]])
+order_products__prior = read.csv(file_path[[3]])
+order_products__train = read.csv(file_path[[4]])
+orders = read.csv(file_path[[5]])
+products = read.csv(file_path[[6]])
+
