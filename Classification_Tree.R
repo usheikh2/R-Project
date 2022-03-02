7e6a906a2b877f226a12ccee8621b918bd770447
jhoffs <73670820+jhoffs@users.noreply.github.com>
2022-03-02 18:20:25 -0500

    Classification Models

--- Classification_Tree.R
+++ Classification_Tree.R
@@ -5,13 +5,13 @@ library(rpart)
 library(rpart.plot)
 library(tidyverse)
 # Read in CSV file
-spotify2 <- read.csv("spotify_dataset.csv")
+spotify1 <- read.csv("spotify_dataset.csv")
 
 #Remove null values
-spotify2 <- spotify2[complete.cases(spotify),]
+spotify1 <- spotify1[complete.cases(spotify1),]
 
 #Remove unused columns
-spotify <- spotify2[, -c(1,3,4,5,7,9,10,11,12,21,23)]
+spotify <- spotify1[, -c(1,3,4,5,7,9,10,11,12,21,23)]
 
 #Turn convert streams column from chr to numeric
 spotify$Streams <- gsub(",", "",as.character(spotify$Streams))
@@ -24,7 +24,7 @@ spotifyTrain <- spotify[-trainingRows,]
 
 ## Fit First Tree with all Predictors
 fit <- rpart(Highest.Charting.Position ~ ., method = "anova", data = spotifyTrain, control=rpart.control(cp=0.01,maxdepth=10))
-rpart.plot(fit, main="Classification Tree")
+rpart.plot(fit, main="Classification Tree Model 1")
 
 # Test Model 
 pred <- predict(fit, spotifyTest) 
@@ -34,7 +34,7 @@ postResample(pred = pred, obs = spotifyTest$Highest.Charting.Position)
 df <- data.frame(imp = fit$variable.importance)
 df2 <- df %>% 
   tibble::rownames_to_column() %>% 
-  dplyr::rename("variable" = rowname) %>% 
+  dplyr::rename("Variable" = rowname) %>% 
   dplyr::arrange(imp) %>%
   dplyr::mutate(variable = forcats::fct_inorder(variable))
 ggplot2::ggplot(df2) +
@@ -47,7 +47,7 @@ ggplot2::ggplot(df2) +
 ##Fit model with only the best predictor(streams)
 
 fit2 <- rpart(Highest.Charting.Position ~ Streams, method = "anova", data = spotifyTrain)
-rpart.plot(fit2, main="Classification Tree")
+rpart.plot(fit2, main="Classification Tree Model 2")
 
 #Test model 
 pred <- predict(fit2, spotifyTest) 
@@ -56,7 +56,7 @@ postResample(pred = pred, obs = spotifyTest$Highest.Charting.Position)
 ##Fit Model with top 3 Predictors
 
 fit3 <- rpart(Highest.Charting.Position ~ Streams + Artist.Followers + Popularity, method = "anova", data = spotifyTrain)
-rpart.plot(fit3, main="Classification Tree")
+rpart.plot(fit3, main="Classification Tree Model 3")
 
 # Test Model 
 pred <- predict(fit3, spotifyTest) 
