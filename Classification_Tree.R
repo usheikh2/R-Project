9e802746da1bafeddac0fd9899b80ccea9dcb6c0
jhoffs <73670820+jhoffs@users.noreply.github.com>
2022-03-09 16:55:44 -0500

    Update Regression Tree

--- Classification_Tree.R
+++ Classification_Tree.R
@@ -5,13 +5,13 @@ library(rpart)
 library(rpart.plot)
 library(tidyverse)
 # Read in CSV file
-spotify1 <- read.csv("spotify_dataset.csv")
+spotify <- read.csv("spotify_dataset.csv")
 
 #Remove null values
-spotify1 <- spotify1[complete.cases(spotify1),]
+spotify1 <- spotify[complete.cases(spotify1),]
 
 #Remove unused columns
-spotify <- spotify1[, -c(1,3,4,5,7,9,10,11,12,21,23)]
+spotify <- spotify[, -c(1,3,4,5,7,9,10,11,12,21,23)]
 
 #Turn convert streams column from chr to numeric
 spotify$Streams <- gsub(",", "",as.character(spotify$Streams))
@@ -23,11 +23,25 @@ spotifyTest <- spotify[trainingRows,]
 spotifyTrain <- spotify[-trainingRows,]
 
 ## Fit First Tree with all Predictors
-fit <- rpart(Highest.Charting.Position ~ ., method = "anova", data = spotifyTrain, control=rpart.control(cp=0.01,maxdepth=30), preProc = c("center", "scale"))
-rpart.plot(fit, main="Classification Tree Model 1")
+fit <- rpart(Highest.Charting.Position ~ ., method = "anova", data = spotifyTrain, control=rpart.control(cp=.01 ,maxdepth=30))
+rpart.plot(fit, main="Classification Tree Model With All Predictos")
+
+#summary(fit)
+plotcp(fit)
 
 # Test Model 
 pred <- predict(fit, spotifyTest) 
+postResample(pred = pred, obs = spotifyTest$Highest.Charting.Position)
+
+#Change Complexity
+fit2 <- rpart(Highest.Charting.Position ~ ., method = "anova", data = spotifyTrain, control=rpart.control(cp=.032 ,maxdepth=30))
+rpart.plot(fit2, main="Classification Tree Model Reduced Complexity")
+
+#summary(fit2)
+plotcp(fit2)
+
+# Test Model 
+pred <- predict(fit2, spotifyTest) 
 postResample(pred = pred, obs = spotifyTest$Highest.Charting.Position)
 
 # Plot predictor importance for future models
@@ -44,31 +58,8 @@ ggplot2::ggplot(df2) +
   scale_fill_grey() +
   theme_bw()
 
-#Prune Model
-pfit<- prune(fit, cp = fit$cptable[which.min(fit$cptable[,"xerror"]),"CP"])
-rpart.plot(pfit, main="Classification Tree Model 1 Post Prune")
-pred <- predict(pfit, spotifyTest) 
-postResample(pred = pred, obs = spotifyTest$Highest.Charting.Position)
-pfit
 
 
-##Fit model with only the best predictor(streams)
 
-fit2 <- rpart(Highest.Charting.Position ~ Streams, method = "anova", data = spotifyTrain, control=rpart.control(cp=0.01,maxdepth=30))
-fit2 <- prune(fit2, cp = fit$cptable[which.min(fit$cptable[,"xerror"]),"CP"])
-rpart.plot(fit2, main="Classification Tree Model 2")
 
-#Test model 
-pred <- predict(fit2, spotifyTest) 
-postResample(pred = pred, obs = spotifyTest$Highest.Charting.Position)
-
-##Fit Model with top 3 Predictors
-
-fit3 <- rpart(Highest.Charting.Position ~ Streams + Artist.Followers + Popularity, method = "anova", data = spotifyTrain, control=rpart.control(cp=0.01,maxdepth=30))
-fit3 <- prune(fit3, cp = fit$cptable[which.min(fit$cptable[,"xerror"]),"CP"])
-rpart.plot(fit3, main="Classification Tree Model 3")
-
-# Test Model 
-pred <- predict(fit3, spotifyTest) 
-postResample(pred = pred, obs = spotifyTest$Highest.Charting.Position)
 
